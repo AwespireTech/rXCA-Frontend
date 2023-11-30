44958b0684712c7c091ec4c9b605efe8ce09686d
akaSwap <akaswap.com@gmail.com>
2023-11-30 14:52:24 +0800

    feat: add homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,24 @@
+import Button from "@/components/atoms/button"
 import Explore from "./explore/page"
+import Link from "next/link"
+import lang from "@/lang/zh"
+import { PHYSICAL_CARD_LINK } from "@/constant"
 
 export default function Home() {
-  return <Explore />
+  return (
+    <div className="flex gap-16 py-64">
+      <Link
+        className="box-border rounded-full border border-white bg-highlight px-10 py-3 text-3xl text-white"
+        href="/explore"
+      >
+        {lang.page.home.daostar}
+      </Link>
+      <Link
+        className="box-border rounded-full border border-white bg-highlight px-10 py-3 text-3xl text-white"
+        href={PHYSICAL_CARD_LINK}
+      >
+        {lang.page.home.card}
+      </Link>
+    </div>
+  )
 }
